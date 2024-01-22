81182cefa2771bb565905da41ed322d4b22b273a
Soroso <tudgus010707@naver.com>
2024-01-22 18:37:25 +0900

    첫 커밋 & Nav바 추가

---
+++ my-app/src/App.jsx
@@ -0,0 +1,39 @@
+// import { useEffect, useState } from "react";
+// import { createClient } from "@supabase/supabase-js";
+import NavBar from "./components/NavBar";
+
+// const supabaseUrl = "https://zwxllgrjrwjibqywmpxv.supabase.co";
+// const supabaseAconKey = import.meta.env.VITE_SUPABASE_KEY;
+// const supabase = createClient(supabaseUrl, supabaseAconKey);
+
+function App() {
+  // const [todoContents, setTodoContents] = useState([]);
+
+  // useEffect(() => {
+  //   getTodoContents();
+  //   console.log(todoContents);
+  // }, []);
+
+  // async function getTodoContents() {
+  //   let { data: table, error } = await supabase.from("page2").select("*");
+  //   setTodoContents(table);
+  // }
+  // async function refreshHistory() {
+  //   let { data: table, error } = await supabase.from("page2").select("*");ㄷ
+  //   setTodoContents(t)
+  // }
+  // async function addTodoContent() {
+  //   const { data, error } = await supabase
+  //     .from("page2")
+  //     .insert([{ todo: prompt("What to do?") }])
+  //     .select();
+  //   getTodoContents();
+  // }
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
