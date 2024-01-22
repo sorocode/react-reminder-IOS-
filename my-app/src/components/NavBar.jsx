7af4d3c2a0ecef5bcad6e5222d6a19627ef37d15
Soroso <tudgus010707@naver.com>
2024-01-22 18:45:17 +0900

    목록 텍스트 컬러 변경

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 export default function NavBar() {
   return (
     <div className="flex flex-row justify-between my-3 mx-2">
-      <div>&lt; 목록</div>
+      <div className="text-blue-600 font-bold">&lt; 목록</div>
       <div className="flex">
         <div className="mx-2">
           <svg
